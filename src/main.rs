536709bb44c9e1c1ea31e9466ddea297cca23843
joseporcar <joseaporcar@gmail.com>
2025-03-06 10:30:25 -0400

    very basics done!!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Label};
+
+const APP_ID: &str = "org.gtk_rs.TodoMatrix";
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+
+    app.run()
+}
+fn build_ui(app: &Application) {
+    let label = Label::builder().label("label").build();
+
+    ApplicationWindow::builder().application(app)
+        .title("Todo—Matrix")
+        .child(&label)
+        .build().present();
 }
