ba413f7ec1dc8b927ee85405eca694c15c42adc0
joseporcar <joseaporcar@gmail.com>
2025-03-07 12:16:35 -0400

    gave equality to the importance and urgency stuffs

--- src/task_matrix.rs
+++ src/task_matrix.rs
@@ -2,16 +2,36 @@ pub struct Matrix {
     tasks: Vec<Vec<Option<Task>>>
 }
 impl Matrix {
-    pub fn empty() -> Matrix {
-        Matrix { tasks: vec![vec![None; 5]; 5] }
-    }
+    // pub fn empty() -> Matrix {
+    //     Matrix { tasks: vec![vec![None; 5]; 5] }
+    // }
 }
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct Task {
     content: String,
     completed: bool,
+    importance: Importance,
+    urgency: Urgency,
 }
 
 impl Task {
 
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
+enum Importance {
+    Low,
+    MidLo,
+    Mid,
+    MidHigh,
+    High
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
+enum Urgency {
+    Low,
+    MidLo,
+    Mid,
+    MidHigh,
+    High
 }
