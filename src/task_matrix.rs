e4eac3e2033047301215a799da3234cfe09c41af
joseporcar <joseaporcar@gmail.com>
2025-03-10 10:36:51 -0400

    added from vec method

--- src/task_matrix.rs
+++ src/task_matrix.rs
@@ -5,14 +5,23 @@ impl <'a> Matrix {
     pub fn empty() -> Matrix {
         Matrix { tasks: Vec::new() }
     }
-    pub fn get_tasks(&'a self) -> &'a Vec<Task> {
+    pub fn tasks(&self) -> &Vec<Task> {
         &self.tasks
+    }
+    pub fn task(&self, index:usize) -> &Task {
+        &self.tasks[index]
     }
     pub fn sort_by_importance(&mut self) {
         self.tasks.sort_by(|a, b| a.cmp_importance(b));
     }
     pub fn sort_by_urgency(&mut self) {
         self.tasks.sort_by(|a, b| a.cmp_urgency(b));
+    }
+}
+
+impl From<Vec<Task>> for Matrix {
+    fn from(value: Vec<Task>) -> Self {
+        Matrix { tasks: value}
     }
 }
 #[derive(Debug)]
@@ -24,18 +33,12 @@ pub struct Task {
 }
 
 impl Task {
-    pub fn cmp_importance(&self, other: &Task) -> std::cmp::Ordering {
+    fn cmp_importance(&self, other: &Task) -> std::cmp::Ordering {
         self.importance.cmp(&other.importance)
     }
 
-    pub fn cmp_urgency(&self, other: &Task) -> std::cmp::Ordering {
+    fn cmp_urgency(&self, other: &Task) -> std::cmp::Ordering {
         self.urgency.cmp(&other.urgency)
-    }
-
-    pub fn _test() {
-        let task1 = Task { content: "h".to_owned(), completed: true, importance: Importance::High, urgency: Urgency::High};
-        let task2 = Task { content: "h".to_owned(), completed: true, importance: Importance::MidLow, urgency: Urgency::High};
-        println!("{:?}", task1.cmp_importance(&task2))
     }
 }
 
