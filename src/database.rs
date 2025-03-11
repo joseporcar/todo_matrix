286b16475b15a334393e4798b554cdfb6fef9a20
joseporcar <joseaporcar@gmail.com>
2025-03-11 17:46:12 -0400

    Begin building database access. Only have openconnection and create_table

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,24 @@
 use rusqlite::{Connection, Result};
 
-pub fn open_conection() -> Connection {
-    // TODO LINK TO ACTUAL FILE NOT JUST MEM
-    Connection::open_in_memory().expect("Failed to open database")
+pub struct Table {
+    connection: Connection
 }
+impl Table {
+    pub fn open_conection() -> Connection {
+        // TODO LINK TO ACTUAL FILE NOT JUST MEM
+        Connection::open_in_memory().expect("Failed to open database")
+    }
+    pub fn create_table(&self) {
+        self.connection.execute_batch("
+            CREATE TABLE task (
+                date        TEXT;
+                content     TEXT;
+                complete    INTEGER;
+                importance  INTEGER;
+                urgency     INTEGER;
+            )
+        ").expect("Error at creating table");
+    }
+}
+
+
