09fe0cf19e55da89249c746996da8e58893199f0
joseporcar <joseaporcar@gmail.com>
2025-03-18 09:44:13 -0400

    added conversion layer for urgency

--- src/task_matrix.rs
+++ src/task_matrix.rs
@@ -147,6 +147,18 @@ pub enum Urgency {
     MidHigh,
     High,
 }
+impl From<&[u8]> for Urgency {
+    fn from(value: &[u8]) -> Self {
+        match value {
+            b"Low" => Urgency::Low,
+            b"MidLow" => Urgency::MidLow,
+            b"Mid" => Urgency::Mid,
+            b"MidHigh" => Urgency::MidHigh,
+            b"High" => Urgency::High,
+            _ => Urgency::Low,
+        }
+    }
+}
 impl Display for Urgency {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self)
@@ -156,6 +168,15 @@ impl Display for Urgency {
 impl ToSql for Urgency {
     fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {
         Ok(ToSqlOutput::from(self.to_string()))
+    }
+}
+
+impl FromSql for Urgency {
+    fn column_result(value: rusqlite::types::ValueRef<'_>) -> rusqlite::types::FromSqlResult<Self> {
+        match value {
+            rusqlite::types::ValueRef::Text(items) => Ok(items.into()),
+            _ => Err(rusqlite::types::FromSqlError::InvalidType)
+        }
     }
 }
 
