63e5680ef15514bcc0632f2543efce52ffdca610
joseporcar <joseaporcar@gmail.com>
2025-03-19 10:13:46 -0400

    added from sql to the rest or the types

--- src/task_matrix.rs
+++ src/task_matrix.rs
@@ -127,6 +127,18 @@ pub enum Importance {
     MidHigh,
     High,
 }
+impl From<&[u8]> for Importance {
+    fn from(value: &[u8]) -> Self {
+        match value {
+            b"Low" => Importance::Low,
+            b"MidLow" => Importance::MidLow,
+            b"Mid" => Importance::Mid,
+            b"MidHigh" => Importance::MidHigh,
+            b"High" => Importance::High,
+            _ => Importance::Low,
+        }
+    }
+}
 impl Display for Importance {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self)
@@ -136,6 +148,15 @@ impl Display for Importance {
 impl ToSql for Importance {
     fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {
         Ok(ToSqlOutput::from(self.to_string()))
+    }
+}
+
+impl FromSql for Importance {
+    fn column_result(value: rusqlite::types::ValueRef<'_>) -> rusqlite::types::FromSqlResult<Self> {
+        match value {
+            rusqlite::types::ValueRef::Text(items) => Ok(items.into()),
+            _ => Err(rusqlite::types::FromSqlError::InvalidType)
+        }
     }
 }
 
