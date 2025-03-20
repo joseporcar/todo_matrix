660aee8f0dd6c13de698c68fdd0aefd4ad9ce0a9
joseporcar <joseaporcar@gmail.com>
2025-03-20 08:30:43 -0400

    the basic scheme should now be done

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{self, glib};
+use gtk::{self, glib, Orientation};
 
 pub fn run_app(app_id: &str) -> glib::ExitCode {
     let app = build_app(app_id);
@@ -24,10 +24,22 @@ fn build_ui(app: &gtk::Application) {
 }
 
 fn main_box() -> gtk::Box {
-
+    let main_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+    main_box.append(&top_bar());
+    main_box.append(&center_box());
+    main_box.append(&horizontal_labels());
     todo!()
 }
-
+fn center_box() -> gtk::Box {
+    let center_box = gtk::Box::builder()
+        .orientation(Orientation::Horizontal)
+        .build();
+    center_box.append(&vertical_labels());
+    center_box.append(&content_box());
+    center_box
+}
 fn vertical_labels() -> gtk::Box {
     todo!()
 }
@@ -36,7 +48,7 @@ fn horizontal_labels() -> gtk::Box {
     todo!()
 }
 
-fn display_box() -> gtk::Box {
+fn content_box() -> gtk::Box {
     todo!()
 }
 
