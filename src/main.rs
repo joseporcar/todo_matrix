caa7738190fd84aea29343b395d01803c985032a
joseporcar <joseaporcar@gmail.com>
2025-03-24 13:57:05 -0400

    changed appid to be more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use todo_matrix::run_app;
 
-const APP_ID: &str = "gtk.personal.TodoMatrix";
+const APP_ID: &str = "com.github.joseporcar.TodoMatrix";
 fn main() {
     run_app(APP_ID);
     //println!("{:?}", todo_matrix::testing())
